916966a1933e60913481f5c1b2dcfa11d94351ce
gunturdarmawan <19523166@student.uii.ac.id>
2021-11-30 00:20:32 +0700

    Connect with Firebase

--- app/src/main/java/com/example/edulock/onboarding/splashScreen.kt
+++ app/src/main/java/com/example/edulock/onboarding/splashScreen.kt
@@ -4,21 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import com.example.edulock.R
 
 class splashScreen : AppCompatActivity() {
 
-    lateinit var handler: Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-        handler = Handler()
-        handler.postDelayed({
-
+        Handler(Looper.getMainLooper()).postDelayed({
             // Delay and Start Activity
             val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
             finish()
-        } , 2500)
+        } , 2000)
     }
 }
